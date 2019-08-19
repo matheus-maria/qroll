b514ad07044921e9b9a79793426e4ee730f8d511
Matheus Maria <matheus.maria@oransys.net>
2019-08-19 16:03:38 -0300

    Modfys to use docker-compose

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = 3000
 const HOST = '0.0.0.0'
 
 const app = express()
+
+//Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
 
 app.get('/', (req,res) => {
     res.send('Qroll FTT !')
