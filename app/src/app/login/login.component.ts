ade334369cc2609b6233e07cd34412cbbf1fae53
Matheus Maria <matheus.maria@oransys.net>
2019-09-30 11:29:13 -0300

    Login Screen created

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+export class PasswordValidation {
+
+  static MatchPassword(AC: AbstractControl) {
+     let password = AC.get('password').value; // to get value in input tag
+     let confirmPassword = AC.get('verifyPassword').value; // to get value in input tag
+      if(password != confirmPassword) {
+          AC.get('verifyPassword').setErrors( {MatchPassword: true} )
+      } else {
+          return null
+      }
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +22,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private fb: FormBuilder) { }
+
+  public form: FormGroup
+  public registerForm: FormGroup
+
+
+  register: boolean = false
 
   ngOnInit() {
+    
+    this.form = this.fb.group({
+      ra : ['',Validators.required],
+      password : ['',Validators.required]
+    })
+
+    this.registerForm = this.fb.group({
+      ra : ['',Validators.required],
+      password : ['',Validators.required],
+      verifyPassword : ['',Validators.required]
+    },{
+      validators: PasswordValidation.MatchPassword
+    })
+
   }
 
 }
