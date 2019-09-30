8061e820a37a857b367a4b7ef6d93cb77a517bce
Matheus Maria <matheus.maria@oransys.net>
2019-09-30 09:33:04 -0300

    imports

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
+/* MATERIAL DESIGN */
+import {
+  MatCardModule, MatMenuModule,
+  MatButtonModule, MatInputModule,
+  MatIconModule, MatIconRegistry, MatSnackBarModule, MatSelectModule, MatDialogModule, MatProgressSpinnerModule, MatToolbarModule, MatTableModule, MatCheckboxModule, MatProgressBarModule, MatDividerModule, MatTooltipModule, MatListModule, MatBadgeModule, MatChipsModule, MatSliderModule, MatTabsModule, MatExpansionModule, MatAutocompleteModule, MatDatepickerModule, MatNativeDateModule, MatSlideToggleModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,11 +22,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    CommonModule, FormsModule, ReactiveFormsModule,    
+    BrowserModule, BrowserAnimationsModule, AppRoutingModule,
+    HttpClientModule,
+    // Angular Material    
+    MatTabsModule, MatCardModule, MatToolbarModule, MatMenuModule, MatSnackBarModule, MatDialogModule, MatTableModule, MatListModule, MatBadgeModule, MatExpansionModule,
+    MatButtonModule, MatInputModule, MatSelectModule, MatCheckboxModule, MatChipsModule,
+    MatProgressSpinnerModule, MatProgressBarModule, MatDividerModule, MatTooltipModule, MatSliderModule, 
+    MatIconModule,MatAutocompleteModule,MatDatepickerModule, MatNativeDateModule, MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private matIconRegistry: MatIconRegistry) {
+    this.matIconRegistry.setDefaultFontSetClass("fas");
+ }
+}
